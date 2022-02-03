73a0d61a643426ce72ad575e171c858e316110d1
PS <89146019257@mail.ru>
2022-02-04 01:25:17 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // входные данные
+        int price = 230;
+        int amount = 20;
+        int miles = price / amount;
+
+        System.out.println("Количество бонусных миль");
+        System.out.println(miles);
+
+    }
+}
